bf3fa64e666469a269cf21d78b63fea874edd4ed
jjb <jaro.baj@o2.pl>
2025-07-28 11:43:46 +0200

    Converted to PropertyBasedBitmapEffect.
    Deleted BackgroundRemoverConfigDialog in favor of conversion to PropertyBasedBitmapEffect. Added post-build event to copy the effect to the Paint.NET effects directory. Improved task cancellation. Contributor: _koh_ via Paint.NET forums.

--- BackgroundRemover/Effects/BackgroundRemoverEffect.cs
+++ BackgroundRemover/Effects/BackgroundRemoverEffect.cs
@@ -1,18 +1,21 @@
-﻿using BackgroundRemover.Utils;
-using Dialogs;
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 using PaintDotNet;
 using PaintDotNet.Effects;
 using PaintDotNet.Imaging;
+using PaintDotNet.IndirectUI;
+using PaintDotNet.PropertySystem;
 using System;
 using System.Drawing;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace BackgroundRemover.Effects;
 
 [PluginSupportInfo(typeof(PluginSupportInfo))]
-public class BackgroundRemoverEffect : BitmapEffect
+public class BackgroundRemoverEffect : PropertyBasedBitmapEffect
 {
     private const string IconResourcePath = "BackgroundRemover.Assets.ic_fluent_video_background_effect_24_filled.ico";
     private const string ONNX16FileName = "RMBG-2.0_FP16.onnx";
@@ -20,73 +23,150 @@ public class BackgroundRemoverEffect : BitmapEffect
 
     private const string ErrorDialogTitle = "Background Remover Error";
 
-    private bool _isFinalCall = false;
-    private BackgroundRemoverConfig? _config;
-    private IEffectInputBitmap<ColorBgra32>? _sourceBitmap;
+    private readonly string _onnxPathFp16;
+    private readonly string _onnxPathFp32;
 
-    public BackgroundRemoverEffect() : base("Background Remover", LoadIcon(), "AI Tools", BitmapEffectOptionsFactory.Create() with { IsConfigurable = true })
-    { }
-
-    private static Bitmap LoadIcon()
+    private static Bitmap Icon
     {
-        var assembly = Assembly.GetExecutingAssembly();
-        using var iconStream = assembly.GetManifestResourceStream(IconResourcePath)!;
-        return new Bitmap(iconStream);
+        get
+        {
+            if (null == field)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                using var iconStream = assembly.GetManifestResourceStream(IconResourcePath)!;
+                field = new Bitmap(iconStream);
+            }
+
+            return field;
+        }
     }
 
-    protected override IEffectConfigForm OnCreateConfigForm() => new BackgroundRemoverConfigDialog();
-
-    protected override void OnSetToken(EffectConfigToken? newToken) => _config = newToken as BackgroundRemoverConfig;
-
-    protected override void OnInitializeRenderInfo(IBitmapEffectRenderInfo renderInfo)
+    public BackgroundRemoverEffect() : base("Background Remover", Icon, SubmenuNames.Photo, BitmapEffectOptionsFactory.Create() with { IsConfigurable = true })
     {
-        renderInfo.OutputPixelFormat = PixelFormats.Bgra32;
-        renderInfo.Schedule = BitmapEffectRenderingSchedule.None;
-        renderInfo.Flags = BitmapEffectRenderingFlags.DisableSelectionClipping;
-        _sourceBitmap = Environment.GetSourceBitmapBgra32();
+        var baseDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+        _onnxPathFp16 = Path.Combine(baseDir, "onnx", ONNX16FileName);
+        _onnxPathFp32 = Path.Combine(baseDir, "onnx", ONNX32FileName);
+
+        if (!File.Exists(_onnxPathFp16) || !File.Exists(_onnxPathFp32))
+            ShowErrorDialog("Required ONNX models missing. Please ensure both FP16 and FP32 models are in the 'onnx' folder.");
+    }
+
+    // properties
+    private enum PropertyName
+    {
+        FP16,
+        GPU,
+    }
+
+    protected override PropertyCollection OnCreatePropertyCollection() => new([
+        new BooleanProperty(PropertyName.FP16, false),
+        new BooleanProperty(PropertyName.GPU,  false),
+    ]);
+
+    protected override ControlInfo OnCreateConfigUI(PropertyCollection props)
+    {
+        var config = CreateDefaultConfigUI(props);
+
+        config.SetPropertyControlValue(PropertyName.FP16, ControlInfoPropertyNames.DisplayName, "Use FP16");
+        config.SetPropertyControlValue(PropertyName.FP16, ControlInfoPropertyNames.Description, "Enable FP16 computation to reduce memory usage and potentially improve performance.");
+        config.SetPropertyControlValue(PropertyName.FP16, ControlInfoPropertyNames.ShowHeaderLine, false);
+
+        config.SetPropertyControlValue(PropertyName.GPU, ControlInfoPropertyNames.DisplayName, "Use GPU");
+        config.SetPropertyControlValue(PropertyName.GPU, ControlInfoPropertyNames.Description, "Enable GPU acceleration to accelerate background removal.");
+        config.SetPropertyControlValue(PropertyName.GPU, ControlInfoPropertyNames.ShowHeaderLine, false);
+
+        return config;
+    }
+
+    // rendering
+    private IEffectInputBitmap<ColorBgra32>? _source;
+    private IBitmap<ColorBgra32>? _bitmap;
+
+    private readonly Lock _lock = new();
+
+    protected override void OnSetToken(PropertyBasedEffectConfigToken? token)
+    {
+        var useFp16 = (bool)token!.GetProperty(PropertyName.FP16)!.Value!;
+        var useGpu = (bool)token.GetProperty(PropertyName.GPU)!.Value!;
+
+        lock (_lock) // ensure thread safety when accessing bitmaps
+            RemoveBackground(useFp16, useGpu);
+    }
+
+    private void RemoveBackground(bool useFp16, bool useGpu)
+    {
+        using var sessionOptions = new SessionOptions { LogSeverityLevel = OrtLoggingLevel.ORT_LOGGING_LEVEL_FATAL };
+        using var runOptions = new RunOptions();
+        var onnx = useFp16 ? _onnxPathFp16 : _onnxPathFp32;
+
+        if (useGpu)
+            sessionOptions.AppendExecutionProvider_DML(0);
+
+        _source ??= Environment.GetSourceBitmapBgra32();
+        using var data = _source.CreateScaler(new(1024, 1024), BitmapInterpolationMode.HighQualityCubic).ToBitmap();
+        using var datL = data.Lock(new(0, 0, data.Size), BitmapLockOptions.ReadWrite);
+        var dat = datL.AsRegionPtr();
+
+        var color = new DenseTensor<float>([1, 3, dat.Height, dat.Width]);
+        var alpha = new DenseTensor<float>([1, 1, dat.Height, dat.Width]);
+        using var vcolor = OrtValue.CreateTensorValueFromMemory(OrtMemoryInfo.DefaultInstance, color.Buffer, [1, 3, dat.Height, dat.Width]);
+        using var valpha = OrtValue.CreateTensorValueFromMemory(OrtMemoryInfo.DefaultInstance, alpha.Buffer, [1, 1, dat.Height, dat.Width]);
+
+        for (var y = 0; y < dat.Height; y++)
+        {
+            for (var x = 0; x < dat.Width; x++)
+            {
+                color[0, 0, y, x] = (dat[x, y].R / 255f - 0.485f) / 0.229f;
+                color[0, 1, y, x] = (dat[x, y].G / 255f - 0.456f) / 0.224f;
+                color[0, 2, y, x] = (dat[x, y].B / 255f - 0.406f) / 0.225f;
+            }
+        }
+
+        try
+        {
+            using var session = new InferenceSession(onnx, sessionOptions);
+            using var task = session.RunAsync(runOptions, session.InputNames, [vcolor], session.OutputNames, [valpha]);
+
+            while (!task.IsCompleted)
+            {
+                runOptions.Terminate = IsCancelRequested;
+                Thread.Sleep(100);
+            }
+
+            for (var y = 0; y < dat.Height; y++)
+            {
+                for (var x = 0; x < dat.Width; x++)
+                    dat[x, y].A = (byte)MathF.Round(alpha[0, 0, y, x] * 255f, MidpointRounding.AwayFromZero);
+            }
+        }
+        catch (Exception ex)
+        {
+            ShowErrorDialog($"Background removal failed:\n{ex.Message}\nTry disabling GPU usage and try again.");
+        }
+
+        DisposableUtil.Free(ref _bitmap);
+        _bitmap = data.CreateScaler(_source.Size, BitmapInterpolationMode.HighQualityCubic).ToBitmap();
     }
 
     protected override void OnRender(IBitmapEffectOutput output)
     {
-        // don't render if the effect configuration was not finalized
-        if (_config == null || !_config.Finalize)
-        {
-            CopySourceToOutput(output);
-        }
-        else if (!_isFinalCall)
-        {
-            CopySourceToOutput(output);
-            _isFinalCall = true;
-        }
-        else
-        {
-            var modelPath = Path.Combine(
-                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
-                    "onnx",
-                    _config.UseFP16 ? ONNX16FileName : ONNX32FileName
-                );
+        using var dstL = output.LockBgra32(); var dst = dstL.AsRegionPtr();
+        using var srcL = _source!.Lock(output.Bounds); var src = srcL.AsRegionPtr();
+        using var auxL = _bitmap!.Lock(output.Bounds, BitmapLockOptions.Read); var aux = auxL.AsRegionPtr();
 
-            try
-            {
-                using var remover = new RMBGBackgroundRemover(modelPath, _config.UseFP16, _config.UseGPU);
-                remover.RemoveBackground(_sourceBitmap!, output, CancellationToken);
-            }
-            catch (OperationCanceledException)
-            {
-                // catch silently
-            }
-            catch (Exception ex)
-            {
-                ShowErrorDialog($"Background removal failed: \n{ex.Message}\nTry disabling GPU usage and try again.");
-            }
+        for (var y = 0; y < dst.Height; y++)
+        {
+            for (var x = 0; x < dst.Width; x++)
+                dst[x, y] = new(src[x, y].Bgr, aux[x, y].A);
         }
-    }
-
-    private void CopySourceToOutput(IBitmapEffectOutput output)
-    {
-        using var outputLock = output.Lock<ColorBgra32>();
-        _sourceBitmap!.CopyPixels(outputLock, output.Bounds.Location);
     }
 
     private static void ShowErrorDialog(string message) => MessageBox.Show(message, ErrorDialogTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+    protected override void OnDispose(bool disposing)
+    {
+        DisposableUtil.Free(ref _bitmap, disposing);
+
+        base.OnDispose(disposing);
+    }
 }
--- BackgroundRemover/Properties/AssemblyInfo.cs
+++ BackgroundRemover/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.Versioning;
 [assembly: AssemblyTitle("Background Remover")]
 [assembly: AssemblyCompany("jjb")]
 [assembly: AssemblyProduct("Background Remover")]
-[assembly: AssemblyCopyright("© 2025 jjb")]
+[assembly: AssemblyCopyright("© 2025 jjb & Contributors")]
 [assembly: SupportedOSPlatform("windows")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("a6e27630-0c80-4c65-9f97-eb7940f5e967")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
